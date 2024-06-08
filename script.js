66db67fdbfe07c7b664c7081305e9ed21bf2b43f
Partha Protim Sarkar <parthaprotims681@gmail.com>
2024-06-08 14:03:46 +0530

    Updated UI design

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-function dis(val){
-    document.getElementById("result").value+=val;
-    console.log(val);
+function dis(val) {
+  document.getElementById("result").value += val;
+  console.log(val);
 }
-function solve(){
-    var x=document.getElementById("result").value;
-    var y=eval(x);
-    document.getElementById("result").value=y;
-}
-function clr(){
-    document.getElementById("result").value="";
-}
-function back(){
-    var value = document.getElementById("result").value;
-    document.getElementById("result").value = value.substr(0, value.length - 1);
-}
-/*Function to change the mode*/
-const toggleSwitch = document.querySelector(
-    '.theme-switch input[type="checkbox"]'
-  );
-  function switchTheme(e) {
-    if (e.target.checked) {
-      document.documentElement.setAttribute("data-theme", "dark");
 
-    } else {
-      document.documentElement.setAttribute("data-theme", "light");
-    }
+function solve() {
+  var x = document.getElementById("result").value;
+  try {
+      var y = eval(x);
+      document.getElementById("result").value = y;
+  } catch (error) {
+      document.getElementById("result").value = "Error";
   }
-  
-  toggleSwitch.addEventListener("change", switchTheme, false);
+}
+
+function clr() {
+  document.getElementById("result").value = "";
+}
+
+function back() {
+  var value = document.getElementById("result").value;
+  document.getElementById("result").value = value.substr(0, value.length - 1);
+}
+
+/* Function to change the mode */
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+function switchTheme(e) {
+  if (e.target.checked) {
+      document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+      document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+toggleSwitch.addEventListener("change", switchTheme, false);
